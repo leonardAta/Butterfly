15f25f0384490c42aa763f7355009a3a94a174fa
Leonard Ata <atatangban@live.com>
2024-04-18 02:07:11 -0400

    React Quill-Rich text editor done

--- client/components/forms/CreatePostForm.js
+++ client/components/forms/CreatePostForm.js
@@ -1,16 +1,22 @@
 import { Avatar } from 'antd'
+import dynamic from 'next/dynamic'
+// import ReactQuill from 'react-quill'
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false })
+
+import 'react-quill/dist/quill.snow.css'
 
 const CreatePostForm = ({ content, setContent, postSubmit }) => {
     return (
         <div className='card'>
             <div className='card-body pb-3'>
                 <form className='form-group'>
-                    <textarea
+                    <ReactQuill
+                        theme='snow'
                         value={content}
-                        onChange={(e) => setContent(e.target.value)}
+                        onChange={(e) => setContent(e)}
                         className='form-control'
                         placeholder="What's on your mind?"
-                    ></textarea>
+                    />
                 </form>
             </div>
             <div class='card-footer'>
